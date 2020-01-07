d449018e78e5c1ba26af0899a61cc27515a5511c
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-01-07 17:24:49 +0200

    Update dependencies

---
+++ Assets/UGF.AssetPipeline.Editor.Tests/Asset.Processor/TestAssetProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UGF.AssetPipeline.Editor.Asset.Processor;
+using UnityEngine;
+
+namespace UGF.AssetPipeline.Editor.Tests.Asset.Processor
+{
+    [Serializable]
+    public class TestAssetProcessor : AssetProcessor
+    {
+        [SerializeField] private string m_name = "Test";
+
+        public string Name { get { return m_name; } set { m_name = value; } }
+    }
+}
