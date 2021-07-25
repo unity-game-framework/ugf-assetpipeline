6df9f0598f3fe26f75c12c4fbf8d326e9afac8fc
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-07-25 18:00:32 +0300

    Add tests

---
+++ Assets/UGF.AssetPipeline.Editor.Tests/Folders/TestFolderEditorUtilityMenu.cs
@@ -0,0 +1,20 @@
+﻿using UGF.AssetPipeline.Editor.Folders;
+using UnityEditor;
+
+namespace UGF.AssetPipeline.Editor.Tests.Folders
+{
+    public static class TestFolderEditorUtilityMenu
+    {
+        [MenuItem("Tests/Folders/Include", false, 2000)]
+        private static void IncludeMenu()
+        {
+            FolderEditorUtility.Include("Assets/UGF.AssetPipeline.Editor.Tests/Folders/Test");
+        }
+
+        [MenuItem("Tests/Folders/Exclude", false, 2000)]
+        private static void ExcludeMenu()
+        {
+            FolderEditorUtility.Exclude("Assets/UGF.AssetPipeline.Editor.Tests/Folders/Test");
+        }
+    }
+}
