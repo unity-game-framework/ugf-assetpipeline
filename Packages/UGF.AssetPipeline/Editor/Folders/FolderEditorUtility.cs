34381177dcde9dbffa0f285804ede08826e7b82c
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-07-25 18:00:28 +0300

    Add FolderEditorUtility

---
+++ Packages/UGF.AssetPipeline/Editor/Folders/FolderEditorUtility.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+
+namespace UGF.AssetPipeline.Editor.Folders
+{
+    public static class FolderEditorUtility
+    {
+        private static readonly char[] m_trimIgnoreMark = { '~' };
+
+        public static bool IsIncluded(string path)
+        {
+            if (string.IsNullOrEmpty(path)) throw new ArgumentException("Value cannot be null or empty.", nameof(path));
+
+            return AssetDatabase.IsValidFolder(path);
+        }
+
+        public static void Include(string path)
+        {
+            if (string.IsNullOrEmpty(path)) throw new ArgumentException("Value cannot be null or empty.", nameof(path));
+
+            path = path.TrimEnd(m_trimIgnoreMark);
+
+            Directory.Move($"{path}~", path);
+        }
+
+        public static void Exclude(string path)
+        {
+            if (string.IsNullOrEmpty(path)) throw new ArgumentException("Value cannot be null or empty.", nameof(path));
+
+            path = path.TrimEnd(m_trimIgnoreMark);
+
+            Directory.Move(path, $"{path}~");
+        }
+    }
+}
